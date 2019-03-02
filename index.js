591624097a6633071e9eb91861c04bade6c7eb6b
alpap <al.pap@live.com>
2019-03-02 15:00:46 +0100

    changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let lat = 46.2165586970357
 let long = 7.31383640082898
 let direction = 253
+let miles = 40
+let step = 30
 
-var feetToMeters = feet => feet / 3.2808
+var FeetToMeters = feet => feet / 3.2808
 var MetersToFeet = meters => meters * 3.2808
-var milesToMeters = miles => miles * 1852
+var MilesToMeters = miles => miles * 1852
+var MilesToFeet = miles => MetersToFeet(MilesToMeters(miles))
 
 var degrees_to_radians = degrees => {
   var pi = Math.PI
@@ -13,14 +16,14 @@ var degrees_to_radians = degrees => {
 
 var position = (lat, long, angle, distance) => {
   let X =
-    feetToMeters(distance) * Math.cos(degrees_to_radians(angle)) * 0.00001 + lat
+    FeetToMeters(distance) * Math.cos(degrees_to_radians(angle)) * 0.00001 + lat
   let Y =
-    feetToMeters(distance) * Math.sin(degrees_to_radians(angle)) * 0.00001 +
+    FeetToMeters(distance) * Math.sin(degrees_to_radians(angle)) * 0.00001 +
     long
   return { X, Y }
 }
 
-var calcEdges = (lat, long, angle, feet) => {
+var CalcEdges = (lat, long, angle, feet) => {
   let Mid = position(lat, long, angle, feet)
   let Minus90 = position(lat, long, angle - 90, feet)
   let Plus90 = position(lat, long, angle + 90, feet)
@@ -30,7 +33,7 @@ var calcEdges = (lat, long, angle, feet) => {
 var feet600 = 9601 // 2956
 var feet4000 = 64001 // 19507
 
-var calculate = (
+var CalculateAllEdges = (
   distanceInFeet,
   maxDistanceInFeet,
   stepInMeters,
@@ -48,15 +51,18 @@ var calculate = (
     i <= maxDistanceInFeet;
     i + MetersToFeet(stepInMeters)
   ) {
-    if (i * 0.0625 < 600) {
-      data.small.push(calcEdges(lat, long, angle, i))
-    } else if (i * 0.0625 > 600 && i * 0.0625 <= 2000) {
-      data.medium.push(calcEdges(lat, long, angle, i))
+    if (i * 0.0625 < 300) {
+      data.small.push(CalcEdges(lat, long, angle, 300))
+    } else if (i * 0.0625 > 300 && i * 0.0625 <= 1000) {
+      data.medium.push(CalcEdges(lat, long, angle, i))
     } else if (i * 0.0625 > 2000) {
-      data.large.push(calcEdges(lat, long, angle, i))
+      data.large.push(CalcEdges(lat, long, angle, 1000))
     }
-    return data
   }
+  return data
 }
 
 //46.2165586970357 7.31383640082898
+console.log(
+  CalculateAllEdges(0, MilesToMeters(miles), step, lat, long, direction)
+)
